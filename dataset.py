9010cc226475700f645991ffee60f22f06d8dd25
Kagaya <kagaya85@outlook.com>
2021-09-02 13:37:40 +0800

    fix edge_index tensor format

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 from typing import Callable, List, Optional, Tuple, Union
+
 import torch
 from torch_geometric.data import InMemoryDataset
 from torch_geometric.data.data import Data
@@ -7,13 +8,23 @@ import numpy as np
 import tqdm as tqdm
 import json
 
+import os
+import os.path as osp
+import shutil
 
-class TraceDataset(InMemoryDataset):
-    def __init__(self, root: Optional[str], transform: Optional[Callable], pre_transform: Optional[Callable], pre_filter: Optional[Callable]):
-        super().__init__(root=root, transform=transform,
-                         pre_transform=pre_transform, pre_filter=pre_filter)
+from itertools import repeat, product
+from copy import deepcopy
+import pdb
+
+
+class TraceClusterDataset(InMemoryDataset):
+    def __init__(self, root: Optional[str], transform: Optional[Callable], pre_transform: Optional[Callable], pre_filter: Optional[Callable], aug: Optional[str]):
+        super(TraceClusterDataset, self).__init__(root=root, transform=transform,
+                                                  pre_transform=pre_transform, pre_filter=pre_filter)
 
         self.data, self.slices = torch.load(self.processed_paths[0])
+
+        self.aug = aug
 
     @property
     def raw_file_names(self) -> Union[str, List[str], Tuple]:
@@ -29,19 +40,13 @@ class TraceDataset(InMemoryDataset):
     def process(self):
         data_list = []
 
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-
         with open(self.raw_file_names, "r") as f:
             raw_data = json.load(f)
 
         for trace_id, trace in tqdm(raw_data.items()):
             node_feats = self._get_node_features(trace)
-            # node_label = self._get_node_labels(trace)
             edge_feats = self._get_edge_features(trace)
+            # [from1, from2, from3 ...] [to1, to2, to3 ...]
             edge_index = self._get_adjacency_info(trace)
 
             data = Data(
@@ -51,6 +56,12 @@ class TraceDataset(InMemoryDataset):
                 edge_attr=edge_feats,
             )
             data_list.append(data)
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
 
         datas, slices = self.collate(data_list)
         torch.save((datas, slices), self.processed_paths[0])
@@ -80,8 +91,8 @@ class TraceDataset(InMemoryDataset):
         for from_id, to_list in trace["edges"]:
             feat = []
             for to in to_list:
-                feat.append[to["startTime"]]
-                feat.append(to["duration"])
+                # feat.append[to["startTime"]]
+                feat.append(to["duration"])    # 归一化
             edge_feats.append(feat)
 
         edge_feats = np.asarray(edge_feats)
@@ -91,13 +102,14 @@ class TraceDataset(InMemoryDataset):
         """
         adjacency list
         """
-        adj_list = []
+        adj_list = [[], []]
         for from_id, to_list in trace["edges"]:
             for to in to_list:
                 to_id = to["spanId"]
-                adj_list.append([from_id, to_id])
+                adj_list[0].append(from_id)
+                adj_list[1].append(to_id)
 
-        return adj_list
+        return torch.tensor(adj_list, dtype=torch.long)
 
     def _get_node_labels(self, trace):
         """
@@ -105,4 +117,223 @@ class TraceDataset(InMemoryDataset):
         """
         pass
 
-    # TODO use get methods
+    def get_num_feature(self, idx):
+        data = self.data.__class__()
+
+        if hasattr(self.data, '__num_nodes__'):
+            data.num_nodes = self.data.__num_nodes__[0]
+
+        # 根据 slice 对整个数据集的 data 进行划分，得到每个 graph 的 data
+        for key in self.data.keys:
+            item, slices = self.data[key], self.slices[key]
+            if torch.is_tensor(item):
+                s = list(repeat(slice(None), item.dim()))
+                s[self.data.__cat_dim__(key,
+                                        item)] = slice(slices[0],
+                                                       slices[0 + 1])
+            else:
+                s = slice(slices[idx], slices[idx + 1])
+
+            data[key] = item[s]
+
+        _, num_feature = data.x.size()
+
+        return num_feature
+
+    def get(self, idx):
+        data = self.data.__class__()
+
+        if hasattr(self.data, '__num_nodes__'):
+            data.num_nodes = self.data.__num_nodes__[idx]
+
+        for key in self.data.keys:
+            item, slices = self.data[key], self.slices[key]
+            if torch.is_tensor(item):
+                s = list(repeat(slice(None), item.dim()))
+                s[self.data.__cat_dim__(key,
+                                        item)] = slice(slices[idx],
+                                                       slices[idx + 1])
+            else:
+                s = slice(slices[idx], slices[idx + 1])
+            data[key] = item[s]
+
+        """
+        edge_index = data.edge_index
+        node_num = data.x.size()[0]
+        edge_num = data.edge_index.size()[1]
+        data.edge_index = torch.tensor([[edge_index[0, n], edge_index[1, n]] for n in range(edge_num) if edge_index[0, n] < node_num and edge_index[1, n] < node_num] + [[n, n] for n in range(node_num)], dtype=torch.int64).t()
+        """
+        node_num = data.edge_index.max()
+        sl = torch.tensor([[n, n] for n in range(node_num)]).t()
+        data.edge_index = torch.cat((data.edge_index, sl), dim=1)
+
+        if self.aug == 'dnodes':
+            data_aug = drop_nodes(deepcopy(data))
+        elif self.aug == 'pedges':
+            data_aug = permute_edges(deepcopy(data))
+        elif self.aug == 'subgraph':
+            data_aug = subgraph(deepcopy(data))
+        elif self.aug == 'mask_nodes':
+            data_aug = mask_nodes(deepcopy(data))
+        elif self.aug == 'none':
+            """
+            if data.edge_index.max() > data.x.size()[0]:
+                print(data.edge_index)
+                print(data.x.size())
+                assert False
+            """
+            data_aug = deepcopy(data)
+            data_aug.x = torch.ones((data.edge_index.max()+1, 1))
+
+        elif self.aug == 'random2':
+            n = np.random.randint(2)
+            if n == 0:
+                data_aug = drop_nodes(deepcopy(data))
+            elif n == 1:
+                data_aug = subgraph(deepcopy(data))
+            else:
+                print('sample error')
+                assert False
+
+        elif self.aug == 'random3':
+            n = np.random.randint(3)
+            if n == 0:
+                data_aug = drop_nodes(deepcopy(data))
+            elif n == 1:
+                data_aug = permute_edges(deepcopy(data))
+            elif n == 2:
+                data_aug = subgraph(deepcopy(data))
+            else:
+                print('sample error')
+                assert False
+
+        elif self.aug == 'random4':
+            n = np.random.randint(4)
+            if n == 0:
+                data_aug = drop_nodes(deepcopy(data))
+            elif n == 1:
+                data_aug = permute_edges(deepcopy(data))
+            elif n == 2:
+                data_aug = subgraph(deepcopy(data))
+            elif n == 3:
+                data_aug = mask_nodes(deepcopy(data))
+            else:
+                print('sample error')
+                assert False
+
+        else:
+            print('augmentation error')
+            assert False
+
+        # print(data, data_aug)
+        # assert False
+
+        return data, data_aug
+
+
+def drop_nodes(data):
+
+    node_num, _ = data.x.size()    # x: num_node * num_node_features
+    _, edge_num = data.edge_index.size()    # edge_index: 2 * edge_num
+    drop_num = int(node_num / 10)
+
+    idx_drop = np.random.choice(node_num, drop_num, replace=False)
+    idx_nondrop = [n for n in range(node_num) if not n in idx_drop]
+    idx_dict = {idx_nondrop[n]: n for n in list(range(node_num - drop_num))}
+
+    # data.x = data.x[idx_nondrop]
+    edge_index = data.edge_index.numpy()
+
+    adj = torch.zeros((node_num, node_num))
+    adj[edge_index[0], edge_index[1]] = 1
+    adj[idx_drop, :] = 0
+    adj[:, idx_drop] = 0
+    edge_index = adj.nonzero().t()
+
+    data.edge_index = edge_index
+
+    # edge_index = [[idx_dict[edge_index[0, n]], idx_dict[edge_index[1, n]]] for n in range(edge_num) if (not edge_index[0, n] in idx_drop) and (not edge_index[1, n] in idx_drop)]
+    # edge_index = [[edge_index[0, n], edge_index[1, n]] for n in range(edge_num) if (not edge_index[0, n] in idx_drop) and (not edge_index[1, n] in idx_drop)] + [[n, n] for n in idx_nondrop]
+    # data.edge_index = torch.tensor(edge_index).transpose_(0, 1)
+
+    return data
+
+
+def permute_edges(data):
+
+    node_num, _ = data.x.size()
+    _, edge_num = data.edge_index.size()
+    permute_num = int(edge_num / 10)
+
+    edge_index = data.edge_index.transpose(0, 1).numpy()
+
+    idx_add = np.random.choice(node_num, (permute_num, 2))
+    # idx_add = [[idx_add[0, n], idx_add[1, n]] for n in range(permute_num) if not (idx_add[0, n], idx_add[1, n]) in edge_index]
+
+    # edge_index = np.concatenate((np.array([edge_index[n] for n in range(edge_num) if not n in np.random.choice(edge_num, permute_num, replace=False)]), idx_add), axis=0)
+    # edge_index = np.concatenate((edge_index[np.random.choice(edge_num, edge_num-permute_num, replace=False)], idx_add), axis=0)
+    edge_index = edge_index[np.random.choice(
+        edge_num, edge_num-permute_num, replace=False)]
+    # edge_index = [edge_index[n] for n in range(edge_num) if not n in np.random.choice(edge_num, permute_num, replace=False)] + idx_add
+    data.edge_index = torch.tensor(edge_index).transpose_(0, 1)
+
+    return data
+
+
+def subgraph(data):
+
+    node_num, _ = data.x.size()
+    _, edge_num = data.edge_index.size()
+    sub_num = int(node_num * 0.2)
+
+    edge_index = data.edge_index.numpy()
+
+    idx_sub = [np.random.randint(node_num, size=1)[0]]
+    idx_neigh = set([n for n in edge_index[1][edge_index[0] == idx_sub[0]]])
+
+    count = 0
+    while len(idx_sub) <= sub_num:
+        count = count + 1
+        if count > node_num:
+            break
+        if len(idx_neigh) == 0:
+            break
+        sample_node = np.random.choice(list(idx_neigh))
+        if sample_node in idx_sub:
+            continue
+        idx_sub.append(sample_node)
+        idx_neigh.union(
+            set([n for n in edge_index[1][edge_index[0] == idx_sub[-1]]]))
+
+    idx_drop = [n for n in range(node_num) if not n in idx_sub]
+    idx_nondrop = idx_sub
+    idx_dict = {idx_nondrop[n]: n for n in list(range(len(idx_nondrop)))}
+
+    # data.x = data.x[idx_nondrop]
+    edge_index = data.edge_index.numpy()
+
+    adj = torch.zeros((node_num, node_num))
+    adj[edge_index[0], edge_index[1]] = 1
+    adj[idx_drop, :] = 0
+    adj[:, idx_drop] = 0
+    edge_index = adj.nonzero().t()
+
+    data.edge_index = edge_index
+
+    # edge_index = [[idx_dict[edge_index[0, n]], idx_dict[edge_index[1, n]]] for n in range(edge_num) if (not edge_index[0, n] in idx_drop) and (not edge_index[1, n] in idx_drop)]
+    # edge_index = [[edge_index[0, n], edge_index[1, n]] for n in range(edge_num) if (not edge_index[0, n] in idx_drop) and (not edge_index[1, n] in idx_drop)] + [[n, n] for n in idx_nondrop]
+    # data.edge_index = torch.tensor(edge_index).transpose_(0, 1)
+
+    return data
+
+
+def mask_nodes(data):
+
+    node_num, feat_dim = data.x.size()
+    mask_num = int(node_num / 10)
+
+    idx_mask = np.random.choice(node_num, mask_num, replace=False)
+    data.x[idx_mask] = torch.tensor(np.random.normal(
+        loc=0.5, scale=0.5, size=(mask_num, feat_dim)), dtype=torch.float32)
+
+    return data
