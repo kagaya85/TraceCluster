d5600fdf1ad2d3b3876090587c8ef471bf235c09
Kagaya <kagaya85@outlook.com>
2021-11-10 19:52:58 +0800

    check node exists

--- visualization/preprocess.py
+++ visualization/preprocess.py
@@ -1,10 +1,10 @@
 from os import stat
 from time import daylight
-from py2neo import Node, Relationship, Graph, Subgraph
+from py2neo import Node, Relationship, Graph, Subgraph, NodeMatcher
 from tqdm import tqdm
 import json
 
-preprocessed_file = '../data/preprocessed/wechat/2021-11-10_17-38-57.json'
+preprocessed_file = '../data/preprocessed/wechat/2021-11-10_18-18-19.json'
 
 node_label = 'Operation'
 edge_label = 'Call'
@@ -35,17 +35,37 @@ def main():
                     node_label, id=fromid,
                     traceid=traceid, service=sub[0], name=sub[1]
                 )
+
+                has_node = False
+                for node in nodes:
+                    if node['traceid'] == from_node['traceid'] and node['id'] == from_node['id']:
+                        has_node = True
+                        from_node = node
+                        break
+
+                if not has_node:
+                    nodes.append(from_node)
+
                 to_node = Node(
                     node_label, id=edge['vertexId'],
                     traceid=traceid, service=edge['service'], name=edge['operation']
                 )
+
+                has_node = False
+                for node in nodes:
+                    if node['traceid'] == to_node['traceid'] and node['id'] == to_node['id']:
+                        has_node = True
+                        to_node = node
+                        break
+
+                if not has_node:
+                    nodes.append(to_node)
 
                 call = Relationship(
                     from_node, edge_label, to_node,
                     start_time=edge['startTime'], duration=edge['duration'],
                     is_error=edge['isError']
                 )
-                nodes.extend([from_node, to_node])
                 relationships.append(call)
 
         graph.create(Subgraph(nodes, relationships))
