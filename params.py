9d40964fb8009bdf260727abaa936f1ba33bb7df
Kagaya <kagaya85@outlook.com>
2022-01-12 16:15:34 +0800

    mv preprocess params & add sw ttl env

--- params.py
+++ params.py
@@ -1,3 +1,138 @@
+# preprocess global parameters
+
+data_path_list = [
+    # Normal
+    'normal/normal0822_01/SUCCESS_SpanData2021-08-22_15-43-01.csv',
+    'normal/normal0822_02/SUCCESS2_SpanData2021-08-22_22-04-35.csv',
+    'normal/normal0823/SUCCESS2_SpanData2021-08-23_15-15-08.csv',
+
+    # F01
+    'F01-01/SUCCESSF0101_SpanData2021-08-14_10-22-48.csv',
+    'F01-02/ERROR_F012_SpanData2021-08-14_01-52-43.csv',
+    'F01-03/SUCCESSerrorf0103_SpanData2021-08-16_16-17-08.csv',
+    'F01-04/SUCCESSF0104_SpanData2021-08-14_02-14-51.csv',
+    'F01-05/SUCCESSF0105_SpanData2021-08-14_02-45-59.csv',
+
+    # F02
+    'F02-01/SUCCESS_errorf0201_SpanData2021-08-17_18-25-59.csv',
+    'F02-02/SUCCESS_errorf0202_SpanData2021-08-17_18-47-04.csv',
+    'F02-03/SUCCESS_errorf0203_SpanData2021-08-17_18-54-53.csv',
+    'F02-04/ERROR_SpanData.csv',
+    'F02-05/ERROR_SpanData.csv',
+    'F02-06/ERROR_SpanData.csv',
+
+    # F03
+    'F03-01/ERROR_SpanData.csv',
+    'F03-02/ERROR_SpanData.csv',
+    'F03-03/ERROR_SpanData.csv',
+    'F03-04/ERROR_SpanData.csv',
+    'F03-05/ERROR_SpanData.csv',
+    'F03-06/ERROR_SpanData.csv',
+    'F03-07/ERROR_SpanData.csv',
+    'F03-08/ERROR_SpanData.csv',
+
+    # F04
+    'F04-01/ERROR_SpanData.csv',
+    'F04-02/ERROR_SpanData.csv',
+    'F04-03/ERROR_SpanData.csv',
+    'F04-04/ERROR_SpanData.csv',
+    'F04-05/ERROR_SpanData.csv',
+    'F04-06/ERROR_SpanData.csv',
+    'F04-07/ERROR_SpanData.csv',
+    'F04-08/ERROR_SpanData.csv',
+
+    # F05
+    "F05-02/ERROR_errorf0502_SpanData2021-08-10_13-53-38.csv",
+    "F05-03/ERROR_SpanData2021-08-07_20-34-09.csv",
+    "F05-04/ERROR_SpanData2021-08-07_21-02-22.csv",
+    "F05-05/ERROR_SpanData2021-08-07_21-28-23.csv",
+
+    # F07
+    "F07-01/back0729/ERROR_SpanData2021-07-29_10-36-21.csv",
+    "F07-01/back0729/SUCCESS_SpanData2021-07-29_10-38-09.csv",
+    "F07-01/ERROR_errorf0701_SpanData2021-08-10_14-09-59.csv",
+    "F07-02/back0729/ERROR_SpanData2021-07-29_13-58-37.csv",
+    "F07-02/back0729/SUCCESS_SpanData2021-07-29_13-51-48.csv",
+    "F07-02/ERROR_errorf0702_SpanData2021-08-10_14-33-35.csv",
+    "F07-03/ERROR_SpanData2021-08-07_22-53-33.csv",
+    "F07-04/ERROR_SpanData2021-08-07_23-49-11.csv",
+    "F07-05/ERROR_SpanData2021-08-07_23-57-44.csv",
+
+    # F08
+    "F08-01/ERROR_SpanData2021-07-29_19-15-36.csv",
+    "F08-01/SUCCESS_SpanData2021-07-29_19-16-01.csv",
+    "F08-02/ERROR_SpanData2021-07-30_10-13-04.csv",
+    "F08-02/SUCCESS_SpanData2021-07-30_10-13-46.csv",
+    "F08-03/ERROR_SpanData2021-07-30_12-07-36.csv",
+    "F08-03/SUCCESS_SpanData2021-07-30_12-07-23.csv",
+    "F08-04/ERROR_SpanData2021-07-30_14-20-15.csv",
+    "F08-04/SUCCESS_SpanData2021-07-30_14-22-24.csv",
+    "F08-05/ERROR_SpanData2021-07-30_11-00-30.csv",
+    "F08-05/SUCCESS_SpanData2021-07-30_11-01-05.csv",
+
+    # F11
+    "F11-01/SUCCESSF1101_SpanData2021-08-14_10-18-35.csv",
+    "F11-02/SUCCESSerrorf1102_SpanData2021-08-16_16-57-36.csv",
+    "F11-03/SUCCESSF1103_SpanData2021-08-14_03-04-11.csv",
+    "F11-04/SUCCESSF1104_SpanData2021-08-14_03-35-38.csv",
+    "F11-05/SUCCESSF1105_SpanData2021-08-14_03-38-35.csv",
+
+    # F12
+    "F12-01/ERROR_SpanData2021-08-12_16-17-46.csv",
+    "F12-02/ERROR_SpanData2021-08-12_16-24-54.csv",
+    "F12-03/ERROR_SpanData2021-08-12_16-36-33.csv",
+    "F12-04/ERROR_SpanData2021-08-12_17-04-34.csv",
+    "F12-05/ERROR_SpanData2021-08-12_16-49-08.csv",
+
+    # F13
+    "F13-01/SUCCESSerrorf1301_SpanData2021-08-16_21-01-36.csv",
+    "F13-02/SUCCESS_SpanData2021-08-13_17-34-58.csv",
+    "F13-03/SUCCESSerrorf1303_SpanData2021-08-16_18-55-52.csv",
+    "F13-04/SUCCESSF1304_SpanData2021-08-14_10-50-42.csv",
+    "F13-05/SUCCESSF1305_SpanData2021-08-14_11-13-43.csv",
+
+    # F14
+    "F14-01/SUCCESS_SpanData2021-08-12_14-56-41.csv",
+    "F14-02/SUCCESS_SpanData2021-08-12_15-24-50.csv",
+    "F14-03/SUCCESS_SpanData2021-08-12_15-46-08.csv",
+
+    # F23
+    "F23-01/ERROR_SpanData2021-08-07_20-30-26.csv",
+    "F23-02/ERROR_SpanData2021-08-07_20-51-14.csv",
+    "F23-03/ERROR_SpanData2021-08-07_21-10-11.csv",
+    "F23-04/ERROR_SpanData2021-08-07_21-34-47.csv",
+    "F23-05/ERROR_SpanData2021-08-07_22-02-42.csv",
+
+    # F24
+    "F24-01/ERROR_SpanData.csv",
+    "F24-02/ERROR_SpanData.csv",
+    "F24-03/ERROR_SpanData.csv",
+
+    # F25
+    "F25-01/ERROR_SpanData2021-08-16_11-17-21.csv",
+    "F25-02/ERROR_SpanData2021-08-16_11-21-59.csv",
+    "F25-03/ERROR_SpanData2021-08-16_12-20-59.csv",
+]
+
+mm_data_path_list = [
+    # '5-18/finer_data.json',
+    # '5-18/finer_data2.json',
+    # '8-2/data.json',
+    # '8-3/data.json',
+    # '11-9/data.json',
+    # '11-18/call_graph_2021-11-18_61266.csv'
+    # '11-22/call_graph_2021-11-22_23629.csv'
+    # '11-29/call_graph_2021-11-29_23629.csv',
+    # '12-3/call_graph_2021-12-03_24486.csv'
+    '12-3/call_graph_2021-12-03_23629.csv'
+]
+
+mm_trace_root_list = [
+    # '11-29/click_stream_2021-11-29_23629.csv',
+    # '12-3/click_stream_2021-12-03_24486.csv'
+    '12-3/click_stream_2021-12-03_23629.csv'
+]
+
 chaos_list = {
     0: 'ts-travel-service',
     1: 'ts-ticketinfo-service',
--- preprocess.py
+++ preprocess.py
@@ -16,142 +16,9 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import requests
 import wordninja
 from transformers import AutoTokenizer, AutoConfig, AutoModel
-import torch
+from params import data_path_list, mm_data_path_list, mm_trace_root_list, chaos_list
 
 data_root = '/data/TraceCluster/raw'
-
-data_path_list = [
-    # Normal
-    'normal/normal0822_01/SUCCESS_SpanData2021-08-22_15-43-01.csv',
-    'normal/normal0822_02/SUCCESS2_SpanData2021-08-22_22-04-35.csv',
-    'normal/normal0823/SUCCESS2_SpanData2021-08-23_15-15-08.csv',
-
-    # F01
-    'F01-01/SUCCESSF0101_SpanData2021-08-14_10-22-48.csv',
-    'F01-02/ERROR_F012_SpanData2021-08-14_01-52-43.csv',
-    'F01-03/SUCCESSerrorf0103_SpanData2021-08-16_16-17-08.csv',
-    'F01-04/SUCCESSF0104_SpanData2021-08-14_02-14-51.csv',
-    'F01-05/SUCCESSF0105_SpanData2021-08-14_02-45-59.csv',
-
-    # F02
-    'F02-01/SUCCESS_errorf0201_SpanData2021-08-17_18-25-59.csv',
-    'F02-02/SUCCESS_errorf0202_SpanData2021-08-17_18-47-04.csv',
-    'F02-03/SUCCESS_errorf0203_SpanData2021-08-17_18-54-53.csv',
-    'F02-04/ERROR_SpanData.csv',
-    'F02-05/ERROR_SpanData.csv',
-    'F02-06/ERROR_SpanData.csv',
-
-    # F03
-    'F03-01/ERROR_SpanData.csv',
-    'F03-02/ERROR_SpanData.csv',
-    'F03-03/ERROR_SpanData.csv',
-    'F03-04/ERROR_SpanData.csv',
-    'F03-05/ERROR_SpanData.csv',
-    'F03-06/ERROR_SpanData.csv',
-    'F03-07/ERROR_SpanData.csv',
-    'F03-08/ERROR_SpanData.csv',
-
-    # F04
-    'F04-01/ERROR_SpanData.csv',
-    'F04-02/ERROR_SpanData.csv',
-    'F04-03/ERROR_SpanData.csv',
-    'F04-04/ERROR_SpanData.csv',
-    'F04-05/ERROR_SpanData.csv',
-    'F04-06/ERROR_SpanData.csv',
-    'F04-07/ERROR_SpanData.csv',
-    'F04-08/ERROR_SpanData.csv',
-
-    # F05
-    "F05-02/ERROR_errorf0502_SpanData2021-08-10_13-53-38.csv",
-    "F05-03/ERROR_SpanData2021-08-07_20-34-09.csv",
-    "F05-04/ERROR_SpanData2021-08-07_21-02-22.csv",
-    "F05-05/ERROR_SpanData2021-08-07_21-28-23.csv",
-
-    # F07
-    "F07-01/back0729/ERROR_SpanData2021-07-29_10-36-21.csv",
-    "F07-01/back0729/SUCCESS_SpanData2021-07-29_10-38-09.csv",
-    "F07-01/ERROR_errorf0701_SpanData2021-08-10_14-09-59.csv",
-    "F07-02/back0729/ERROR_SpanData2021-07-29_13-58-37.csv",
-    "F07-02/back0729/SUCCESS_SpanData2021-07-29_13-51-48.csv",
-    "F07-02/ERROR_errorf0702_SpanData2021-08-10_14-33-35.csv",
-    "F07-03/ERROR_SpanData2021-08-07_22-53-33.csv",
-    "F07-04/ERROR_SpanData2021-08-07_23-49-11.csv",
-    "F07-05/ERROR_SpanData2021-08-07_23-57-44.csv",
-
-    # F08
-    "F08-01/ERROR_SpanData2021-07-29_19-15-36.csv",
-    "F08-01/SUCCESS_SpanData2021-07-29_19-16-01.csv",
-    "F08-02/ERROR_SpanData2021-07-30_10-13-04.csv",
-    "F08-02/SUCCESS_SpanData2021-07-30_10-13-46.csv",
-    "F08-03/ERROR_SpanData2021-07-30_12-07-36.csv",
-    "F08-03/SUCCESS_SpanData2021-07-30_12-07-23.csv",
-    "F08-04/ERROR_SpanData2021-07-30_14-20-15.csv",
-    "F08-04/SUCCESS_SpanData2021-07-30_14-22-24.csv",
-    "F08-05/ERROR_SpanData2021-07-30_11-00-30.csv",
-    "F08-05/SUCCESS_SpanData2021-07-30_11-01-05.csv",
-
-    # F11
-    "F11-01/SUCCESSF1101_SpanData2021-08-14_10-18-35.csv",
-    "F11-02/SUCCESSerrorf1102_SpanData2021-08-16_16-57-36.csv",
-    "F11-03/SUCCESSF1103_SpanData2021-08-14_03-04-11.csv",
-    "F11-04/SUCCESSF1104_SpanData2021-08-14_03-35-38.csv",
-    "F11-05/SUCCESSF1105_SpanData2021-08-14_03-38-35.csv",
-
-    # F12
-    "F12-01/ERROR_SpanData2021-08-12_16-17-46.csv",
-    "F12-02/ERROR_SpanData2021-08-12_16-24-54.csv",
-    "F12-03/ERROR_SpanData2021-08-12_16-36-33.csv",
-    "F12-04/ERROR_SpanData2021-08-12_17-04-34.csv",
-    "F12-05/ERROR_SpanData2021-08-12_16-49-08.csv",
-
-    # F13
-    "F13-01/SUCCESSerrorf1301_SpanData2021-08-16_21-01-36.csv",
-    "F13-02/SUCCESS_SpanData2021-08-13_17-34-58.csv",
-    "F13-03/SUCCESSerrorf1303_SpanData2021-08-16_18-55-52.csv",
-    "F13-04/SUCCESSF1304_SpanData2021-08-14_10-50-42.csv",
-    "F13-05/SUCCESSF1305_SpanData2021-08-14_11-13-43.csv",
-
-    # F14
-    "F14-01/SUCCESS_SpanData2021-08-12_14-56-41.csv",
-    "F14-02/SUCCESS_SpanData2021-08-12_15-24-50.csv",
-    "F14-03/SUCCESS_SpanData2021-08-12_15-46-08.csv",
-
-    # F23
-    "F23-01/ERROR_SpanData2021-08-07_20-30-26.csv",
-    "F23-02/ERROR_SpanData2021-08-07_20-51-14.csv",
-    "F23-03/ERROR_SpanData2021-08-07_21-10-11.csv",
-    "F23-04/ERROR_SpanData2021-08-07_21-34-47.csv",
-    "F23-05/ERROR_SpanData2021-08-07_22-02-42.csv",
-
-    # F24
-    "F24-01/ERROR_SpanData.csv",
-    "F24-02/ERROR_SpanData.csv",
-    "F24-03/ERROR_SpanData.csv",
-
-    # F25
-    "F25-01/ERROR_SpanData2021-08-16_11-17-21.csv",
-    "F25-02/ERROR_SpanData2021-08-16_11-21-59.csv",
-    "F25-03/ERROR_SpanData2021-08-16_12-20-59.csv",
-]
-
-mm_data_path_list = [
-    # '5-18/finer_data.json',
-    # '5-18/finer_data2.json',
-    # '8-2/data.json',
-    # '8-3/data.json',
-    # '11-9/data.json',
-    # '11-18/call_graph_2021-11-18_61266.csv'
-    # '11-22/call_graph_2021-11-22_23629.csv'
-    # '11-29/call_graph_2021-11-29_23629.csv',
-    # '12-3/call_graph_2021-12-03_24486.csv'
-    '12-3/call_graph_2021-12-03_23629.csv'
-]
-
-mm_trace_root_list = [
-    # '11-29/click_stream_2021-11-29_23629.csv',
-    # '12-3/click_stream_2021-12-03_24486.csv'
-    '12-3/click_stream_2021-12-03_23629.csv'
-]
 
 time_now_str = str(time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
 
@@ -351,7 +218,7 @@ def load_span() -> List[DataFrame]:
                 filepath, dtype=data_type
             ).drop_duplicates().dropna()
             spans[ITEM.DURATION] = spans[ITEM.END_TIME] - \
-                                   spans[ITEM.START_TIME]
+                spans[ITEM.START_TIME]
             traces = spans.groupby(ITEM.TRACE_ID)
             for k in traces.groups.keys():
                 trace_abnormal[k] = abnormal
@@ -396,7 +263,8 @@ def getSubspanInfo(span: Span, children_span: list[Span]):
     is_Parallel = 0
     time_spans = []
     for child in children_span:
-        time_spans.append({"start": child.startTime, "end": child.startTime + child.duration})
+        time_spans.append(
+            {"start": child.startTime, "end": child.startTime + child.duration})
     time_spans.sort(key=lambda s: s["start"])
     last_time_span = time_spans[0]
     last_length = -1
@@ -407,7 +275,8 @@ def getSubspanInfo(span: Span, children_span: list[Span]):
                 if time_span != time_spans[0]:
                     is_Parallel = 1
                     time_span["start"] = last_time_span["start"]
-                    time_span["end"] = max(time_span["end"], last_time_span["end"])
+                    time_span["end"] = max(
+                        time_span["end"], last_time_span["end"])
                     time_spans.remove(last_time_span)
             last_time_span = time_span
     subspanNum = len(time_spans) + 1
@@ -427,7 +296,8 @@ def calculate_edge_features(current_span: Span, trace_duration: dict, spanChildr
         features["requestDuration"] = 0
         features["responseDuration"] = 0
         features["subspanDuration"] = 0
-        features["timeScale"] = round((current_span.duration / (trace_duration["max"] - trace_duration["min"])), 4)
+        features["timeScale"] = round(
+            (current_span.duration / (trace_duration["max"] - trace_duration["min"])), 4)
         features["subspanNum"] = 0
         features["requestAndResponseDuration"] = 0
         features["isParallel"] = 0
@@ -453,15 +323,19 @@ def calculate_edge_features(current_span: Span, trace_duration: dict, spanChildr
                 grandChild = spanChildrenMap[child.spanId][0]
                 children_duration += grandChild.duration
                 request_duration += (grandChild.startTime - child.startTime)
-                response_duration += (child.duration - request_duration - grandChild.duration)
-                request_and_response_duration += (child.duration - grandChild.duration)
+                response_duration += (child.duration -
+                                      request_duration - grandChild.duration)
+                request_and_response_duration += (
+                    child.duration - grandChild.duration)
         if child.spanType == "Producer":
             if spanChildrenMap.get(child.spanId) is not None:
                 grandChild = spanChildrenMap[child.spanId][0]
                 children_duration += grandChild.duration
                 if grandChild.startTime + grandChild.duration > trace_duration["max"]:
-                    trace_duration["max"] = grandChild.startTime + grandChild.duration
-    subspan_duration, subspan_num, is_parallel = getSubspanInfo(current_span, children_span)
+                    trace_duration["max"] = grandChild.startTime + \
+                        grandChild.duration
+    subspan_duration, subspan_num, is_parallel = getSubspanInfo(
+        current_span, children_span)
     features["callType"] = 0 if current_span.spanType == "Entry" else 1
     features["isParallel"] = is_parallel
     features["statusCode"] = current_span.code
@@ -471,7 +345,8 @@ def calculate_edge_features(current_span: Span, trace_duration: dict, spanChildr
     features["requestAndResponseDuration"] = request_and_response_duration
     features["subspanDuration"] = subspan_duration
     features["subspanNum"] = subspan_num
-    features["timeScale"] = round((current_span.duration / (trace_duration["max"] - trace_duration["min"])), 4)
+    features["timeScale"] = round(
+        (current_span.duration / (trace_duration["max"] - trace_duration["min"])), 4)
     return features
 
 
@@ -551,7 +426,8 @@ def build_sw_graph(trace: List[Span], time_normolize: Callable[[float], float], 
         operation_select_keys = ['childrenSpanNum', 'requestDuration', 'responseDuration',
                                  'requestAndResponseDuration', 'subspanDuration', 'subspanNum',
                                  'duration', 'rawDuration', 'timeScale']
-        features = calculate_edge_features(span, trace_duration, spanChildrenMap)
+        features = calculate_edge_features(
+            span, trace_duration, spanChildrenMap)
         features['duration'] = time_normolize(span.duration)
         features['rawDuration'] = span.duration
         if span.operation not in operation_map.keys():
@@ -925,7 +801,8 @@ def task(ns, idx, divide_word: bool = True):
     for trace_id, trace_data in tqdm(span_data.groupby([ITEM.TRACE_ID]), desc="processing #{:0>2d}".format(idx),
                                      position=pos):
         trace = [Span(raw_span) for idx, raw_span in trace_data.iterrows()]
-        graph, sset = build_graph(trace_process(trace, divide_word), normalize, operation_map)
+        graph, sset = build_graph(trace_process(
+            trace, divide_word), normalize, operation_map)
         if graph == None:
             continue
         graph_map[trace_id] = graph
@@ -1015,7 +892,8 @@ def main():
             for fu in as_completed(fs):
                 (graphs, sset, temp_operation_map) = fu.result()
                 result_map = utils.mergeDict(result_map, graphs)
-                operation_map = utils.mergeOperation(temp_operation_map, operation_map)
+                operation_map = utils.mergeOperation(
+                    temp_operation_map, operation_map)
                 name_set = set.union(name_set, sset)
                 # control the data size
                 if len(result_map) > args.max_num:
