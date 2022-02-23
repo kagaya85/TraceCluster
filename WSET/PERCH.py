d658fb559ddfe540af5fff5b5fed8c0c5e1c2034
zhoutong <zt17888803321@163.com>
2022-02-23 10:58:36 +0800

    add mPERCH

--- WSET/PERCH.py
+++ WSET/PERCH.py
@@ -1,68 +1,140 @@
+import torch
+import json
+import tqdm
+import argparse
+import time
 import numpy as np
 import pandas as pd
 
 from utils import read_json
 from xcluster.src.python.xcluster.models.PNode import PNode
 from xcluster.src.python.xcluster.utils.Graphviz import Graphviz
+from torch_geometric.loader import DataLoader
+from simclr import SIMCLR
+from aug_dataset_mem import TraceDataset
+from torch.utils.data import Subset
 
-import time
 
 
-def create_dataset():
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def create_dataset(embedding, dataset_path):
     global traceID_list
     traceID_fr = open('./newData/test_traceID.txt', 'r')
     S = traceID_fr.read()
     traceID_fr.close()
     traceID_list = [traceID for traceID in S.split(', ')]
 
-    traces = read_json('./newData/preprocessed/0.json')
+    idx_fr = open('./newData/test_idx_url_y.txt', 'r')
+    S = idx_fr.read()
+    idx_fr.close()
+    test_idx = [int(index_item) for index_item in S.split(', ')]
 
-    # Get the traces
-    traces_data = {}
-    api_class = {}
-    for traceID in traceID_list:
-        traces_data[traceID] = traces[traceID]
-        api_class[traceID] = "abnormal" if traces[traceID]["abnormal"]==1 else "normal"
-
-    # with open(path, 'r') as f:
-    #     traces_data = json.load(f)
-
-    print("Finish reading traces.")
-
-    # 所有操作的字母表
-    alphabet = set()
-
-    # 第一次遍历：得到字母表
-    for trace_id, trace in traces_data.items():
-        for start_vertex_id, edges in trace["edges"].items():
-            for edge in edges:
-                if edge["operation"]:
-                    alphabet.add(edge["operation"])
-
-    print("Finish building alphabet, length: %d" % len(alphabet))
-
-    # 第二次遍历：得到每个trace的operation列表，计算字母表中每个操作出现的次数，作为每个trace对应的array
     dataset = []
-    for trace_id, trace in traces_data.items():
-        # 统计trace中每个operation
-        c = []
-        for start_vertex_id, edges in trace["edges"].items():
-            for edge in edges:
-                c.append(edge["operation"])
 
-        vector = []
-        for a in alphabet:
-            vector.append(c.count(a))
+    if embedding == "original":
+        print("embedding method:", embedding)
 
-        dataset.append((np.array(vector), api_class[trace_id], trace_id))
+        traces = read_json('./newData/preprocessed/data.json')
 
-    print("Finish building dataset, length: %d" % len(dataset))
+        # Get the traces
+        traces_data = {}
+        api_class = {}
+        for traceID in traceID_list:
+            traces_data[traceID] = traces[traceID]
+            api_class[traceID] = "abnormal" if traces[traceID]["abnormal"]==1 else "normal"
+
+        # with open(path, 'r') as f:
+        #     traces_data = json.load(f)
+
+        print("Finish reading traces.")
+
+        # 所有操作的字母表
+        alphabet = set()
+
+        # 第一次遍历：得到字母表
+        for trace_id, trace in traces_data.items():
+            for start_vertex_id, edges in trace["edges"].items():
+                for edge in edges:
+                    if edge["operation"]:
+                        alphabet.add(edge["operation"])
+
+        print("Finish building alphabet, length: %d" % len(alphabet))
+
+        # 第二次遍历：得到每个trace的operation列表，计算字母表中每个操作出现的次数，作为每个trace对应的array
+        for trace_id, trace in traces_data.items():
+            # 统计trace中每个operation
+            c = []
+            for start_vertex_id, edges in trace["edges"].items():
+                for edge in edges:
+                    c.append(edge["operation"])
+
+            vector = []
+            for a in alphabet:
+                vector.append(c.count(a))
+
+            dataset.append((np.array(vector), api_class[trace_id], trace_id))
+
+        print("Finish building dataset, length: %d" % len(dataset))
+    
+    elif embedding == "ourMethod":
+        print("embedding method:", embedding)
+
+        # init dataset
+        dataset = TraceDataset(root=args.dataset, aug='none')    # shuffle
+        print("Finish reading traces.")
+
+        # load the pre-trained model
+        with open(args.model_path + 'train_info.json', "r") as f:  # file name not list
+            model_info = json.load(f)
+            num_layers = model_info['num_layers']
+            output_dim = model_info['output_dim']
+            # num_node_features = model_info['num_node_features']
+            # num_edge_features = model_info['num_edge_features']
+            gnn_type = model_info['gnn_type']
+            pooling_type = model_info['pooling_type']
+            aug = model_info['aug']
+            train_idx = model_info['train_idx']
+            # test_idx = model_info['test_idx']
+        print("Finish loading model info.")
+
+        eval_dataset = Subset(dataset, test_idx)
+
+        # init dataloader
+        dataloader = DataLoader(eval_dataset, batch_size=1)    # eval_dataset
+
+        model = SIMCLR(num_layers=num_layers, input_dim=dataset.num_node_features, output_dim=output_dim,
+                   num_edge_attr=dataset.num_edge_features, gnn_type=gnn_type,
+                   pooling_type=pooling_type).to(device)
+        model.load_state_dict(torch.load(args.model_path + '{}_{}'.format(gnn_type, pooling_type) + ".model"))
+        model.eval()
+        print("Finish loading model.")
+
+        for data in tqdm(dataloader):
+            data = data.to(device)
+            vector = model(data.x, data.edge_index, data.edge_attr, data.batch).tolist()[0]
+            dataset.append((np.array(vector), "abnormal" if data[0].y.numpy()[0]==1 else "normal", data[0].trace_id))
+        print("Finish building dataset, length: %d" % len(dataset))
 
     return dataset
 
 
+def arguments():
+    parser = argparse.ArgumentParser(description="PERCH argumentes")
+    parser.add_argument('--embedding', dest='embedding',
+                        help='select the embedding method of traces, eg. original and ourMethod', type=str, default='ourMethod')
+    parser.add_argument('--dataset', dest='dataset',
+                        help='use other preprocessed data dirpath, eg. ./newData or ./newData/outfiles2022-02-13_21-04-20', default="./newData")
+    parser.add_argument('--model_path', dest='model_path',
+                        default='./newData/model/', help='weights save path')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    dataset = create_dataset()
+    args = arguments()
+
+    dataset = create_dataset(embedding=args.embedding, dataset_path=args.dataset)
 
     root = PNode(exact_dist_thres=10)
 
@@ -87,7 +159,7 @@ if __name__ == '__main__':
     # 记录实验结果 trace_id    label    p
     # open test result
     time_str = str(time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
-    res_f = open('./result_' + time_str + '.txt', 'w')
+    res_f = open('./result_PERCH_' + args.embedding + '_' + time_str + '.txt', 'w')
     for traceID in traceID_list:
         res_content = traceID + '\t' + results[traceID][0] + '\t' + str(results[traceID][1]) + '\n'
         res_f.write(res_content)
---
+++ WSET/plt_1.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt 
+import json
+import pickle
+from scipy.io import loadmat
+import pdb 
+import numpy as np
+'''
+rcnn_car = pickle.load(open('/home/tiankun/pro/pytorch-FPN/output/res101/DETRACvoc_test/default/res101_faster_rcnn_iter_120000/car_pr.pkl','rb'))
+rcnn_bus = pickle.load(open('/home/tiankun/pro/pytorch-FPN/output/res101/DETRACvoc_test/default/res101_faster_rcnn_iter_120000/bus_pr.pkl','rb'))
+rcnn_van = pickle.load(open('/home/tiankun/pro/pytorch-FPN/output/res101/DETRACvoc_test/default/res101_faster_rcnn_iter_120000/van_pr.pkl','rb'))
+rcnn_motor = pickle.load(open('/home/tiankun/pro/pytorch-FPN/output/res101/DETRACvoc_test/default/res101_faster_rcnn_iter_120000/motor_pr.pkl','rb'))
+
+with open("yolo_recall.json",'r') as load_f:
+    RTK = json.load(load_f)
+
+with open("yolo_precision.json",'r') as load_f:
+    PTK = json.load(load_f)
+'''
+'''
+# Faster curve
+plt.figure(1)
+plt.title('PR Curve')
+plt.plot(rcnn_car['rec'],rcnn_car['prec'],color='red',label='car',linewidth=3)
+plt.plot(rcnn_bus['rec'],rcnn_bus['prec'],color='limegreen',label='bus',linewidth=3)
+plt.plot(rcnn_van['rec'],rcnn_van['prec'],color='skyblue',label='van',linewidth=3)
+plt.plot(rcnn_motor['rec'],rcnn_motor['prec'],color='orange',label='motor',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show() 
+'''
+'''
+# YOLO curve
+plt.figure(1)
+plt.title('PR Curve')
+plt.subplot(1,3,1)
+plt.plot(RTK['car'],PTK['car'])
+plt.subplot(1,3,2)
+plt.plot(RTK['bus'],PTK['bus'])
+plt.subplot(1,3,3)
+plt.plot(RTK['van'],PTK['van'])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()
+'''
+'''
+exp1 = loadmat('exp1.mat')
+exp3 = loadmat('exp3.mat')
+exp5 = loadmat('exp5.mat')
+exp5sub1 = loadmat('exp5sub1.mat')
+exp5sub2 = loadmat('exp5sub2.mat')
+exp6 = loadmat('exp6.mat')
+
+x=np.linspace(0.7,1,30)
+
+plt.figure(1)
+plt.title('PRW performance')
+plt.plot(x,exp1['map'][::-1],color='red',label='exp1',linewidth=3)
+plt.plot(x,exp3['map'][::-1],color='limegreen',label='exp3',linewidth=3)
+plt.plot(x,exp5['map'][::-1],color='skyblue',label='exp5',linewidth=3)
+plt.plot(x,exp6['map'][::-1],color='orange',label='exp6',linewidth=3)
+plt.legend()
+plt.xlabel('det_thresh')
+plt.ylabel('mAP')
+plt.show() 
+pdb.set_trace()
+
+
+plt.figure(2)
+plt.title('PR Curve')
+plt.plot(RTK['car'],PTK['car'],color='red',label='car',linewidth=3)
+plt.plot(RTK['bus'],PTK['bus'],color='limegreen',label='bus',linewidth=3)
+plt.plot(RTK['van'],PTK['van'],color='skyblue',label='van',linewidth=3)
+plt.plot(RTK['motor'],PTK['motor'],color='orange',label='motor',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()   
+
+
+
+#contrast curve
+print(rcnn_motor['ap'])
+plt.figure(3)
+plt.title('Motor PR Curve')
+plt.plot(rcnn_motor['rec'],rcnn_motor['prec'],color='red',label='Faster_RCNN',linewidth=3)
+plt.plot(RTK['motor'],PTK['motor'],color='limegreen',label='YOLO',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()  
+
+plt.figure(4)
+plt.title('Car PR Curve')
+plt.plot(rcnn_car['rec'],rcnn_car['prec'],color='red',label='Faster_RCNN',linewidth=3)
+plt.plot(RTK['car'],PTK['car'],color='limegreen',label='YOLO',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()  
+
+plt.figure(5)
+plt.title('Bus PR Curve')
+plt.plot(rcnn_bus['rec'],rcnn_bus['prec'],color='red',label='Faster_RCNN',linewidth=3)
+plt.plot(RTK['bus'],PTK['bus'],color='limegreen',label='YOLO',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()  
+
+plt.figure(6)
+plt.title('Van PR Curve')
+plt.plot(rcnn_van['rec'],rcnn_van['prec'],color='red',label='Faster_RCNN',linewidth=3)
+plt.plot(RTK['van'],PTK['van'],color='limegreen',label='YOLO',linewidth=3)
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()  
+'''
+
+# ax1 = fig.add_subplot(2,1,1)
+# ax2 = fig.add_subplot(2,2,1)
+# ax1 = fig.add_subplot(2,2,1)
+# ax2 = fig.add_subplot(1,2,1)
+
+# original Sieve
+oSieve = []
+abnormal = []
+x = []
+file_oSieve = open("./result_ourMethod_2022-02-22_15-26-40.txt",  "r")
+listOfLines = file_oSieve.readlines()
+file_oSieve.close()
+for idx, line in enumerate(listOfLines):
+    oSieve.append(float(line.strip().split('\t')[3]))
+    x.append(idx+1)
+    if line.split('\t')[1] == "1":
+        abnormal.append(idx+1)
+
+# modified Sieve
+mSieve = []
+file_mSieve = open("./result_ourMethod_2022-02-22_15-26-40.txt",  "r")
+listOfLines = file_mSieve.readlines()
+file_mSieve.close()
+for line in listOfLines:
+    mSieve.append(float(line.strip().split('\t')[3]))
+
+# PERCH
+PERCH = []
+file_PERCH = open("./result_PERCH_2022-02-22_15-39-38.txt",  "r")
+listOfLines = file_PERCH.readlines()
+file_PERCH.close()
+for idx, line in enumerate(listOfLines):
+    if idx >= 178:
+        PERCH.append(float(line.strip().split('\t')[2]))
+
+
+figall=plt.figure(1)
+fig = figall.add_subplot(1,1,1)
+fig.grid(True)
+# fig.plot(x,oSieve,color='purple',label='oSieve',linewidth=1,alpha=0.6,marker='d')
+fig.plot(x,mSieve,color='limegreen',label='mSieve',linewidth=1,alpha=0.6,marker='8')
+fig.plot(x,PERCH,color='orange',label='PERCH',linewidth=1,alpha=0.6,marker='s')
+fig.vlines(abnormal, 0, 1, linestyles='dashed', colors='red')
+legend = fig.legend()
+# fig.legend(loc=1,bbox_to_anchor=(1.05,1.0))
+fig.set_xlabel('samples')
+fig.set_ylabel('P')
+figall.savefig("result.png")
+figall.show()
