ad35ee4335e6010b46c54ce6fcf74d84d1098c2c
yanghong <18018520976@163.com>
2022-02-24 15:25:58 +0800

    update utils.py

--- TraceAnomaly/utils.py
+++ TraceAnomaly/utils.py
@@ -1,6 +1,11 @@
 def get_unique_service_sequence(service_sequence: dict):
+    service_count = {}
+    for v in service_sequence:
+        if service_count.get(v) is None:
+            service_count[v] = 0
+        service_count[v] += 1
     result = ""
     unique_sequence = set(service_sequence)
     for v in sorted(unique_sequence):
-        result = result + v + " "
+        result = result + v + str(service_count[v]) + " "
     return result
